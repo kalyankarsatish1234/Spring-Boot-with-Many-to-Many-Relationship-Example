c029fe827c84bd70449446f2819f96836547b18a
kalyankarsatish1234 <satishkalyankar06@gmail.com>
2024-08-29 18:16:04 +0530

    files updated

---
+++ src/main/java/com/bezkoder/spring/hibernate/manytomany/SpringBootManyToManyApplication.java
@@ -0,0 +1,13 @@
+package com.bezkoder.spring.hibernate.manytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//this is main method
+public class SpringBootManyToManyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootManyToManyApplication.class, args);
+	}
+
+}
